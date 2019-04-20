892ae87e18daa2f48dc8a14c9e896760db578e36
Jeshua Ryzen5 <brayanjeshuavz@gmail.com>
2019-04-20 12:52:49 -0500

    Adding Standart Conexion for XAMPP

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-	$mysqli=new mysqli("localhost","root","","login"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
+	$mysqli=new mysqli("localhost","root","","brokers"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
 
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
